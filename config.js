cbfdaa4bd1d21a596b58c85a8c14b3f45471c7b3
Bastinda <basta.b24@gmail.com>
2026-06-01 10:10:34 +0200

    Add configuration file for environment variables

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    PUBLIC_URL: process.env.PUBLIC_URL || '',
+    DB_URL: process.env.DB_URL,
+    DB_COLLECTION: process.env.DB_COLLECTION || 'expenses',
+    DB_COLLECTION_MISC: process.env.DB_COLLECTION_MISC || 'misc',
+    BOT_TOKEN: process.env.BOT_TOKEN,
+    BOT_NAME: process.env.BOT_NAME || '',
+    BOT_TELEGRAM_USERNAME: process.env.BOT_TELEGRAM_USERNAME || '',
+    WEBHOOK_MODE: process.env.WEBHOOK_MODE === 'true',
+    PORT: parseInt(process.env.PORT) || 3010,
+    BIND_IP4: process.env.BIND_IP4 || '127.0.0.1',
+    ADMINS: JSON.parse(process.env.ADMINS || '[]'),
+    RATE_LIMIT: parseInt(process.env.RATE_LIMIT) || 250,
+    MAINTENANCE_MESSAGE: process.env.MAINTENANCE_MESSAGE || null,
+    ALERT_MESSAGE: process.env.ALERT_MESSAGE || null,
+}
