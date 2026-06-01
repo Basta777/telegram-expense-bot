38244df65fbb07e19fc9751684204a9d55cf585d
Bastinda <basta.b24@gmail.com>
2026-06-01 10:13:45 +0200

    Fix config file path and clean up code

--- db.js
+++ db.js
@@ -1,39 +1,30 @@
 const MongoClient = require('mongodb').MongoClient,
-    config = require('./config.json')
-
+    config = require('./config')
 const client = new MongoClient(config.DB_URL, { useUnifiedTopology: true })
 const collections = {}
-
 async function connect() {
     try {
         await client.connect()
         await client.db().command({ ping: 1 })
         console.log('✅ Connected to database ...')
-
-        // get collections
         collections.expenses = client.db().collection(config.DB_COLLECTION || 'expenses')
         collections.misc = client.db().collection(config.DB_COLLECTION_MISC || 'misc')
-
         return await client.db()
     } catch (e) {
         console.error(e)
         await disconnect()
     }
 }
-
 async function disconnect() {
     await client.close()
     console.log('✅ Disconnected from database ...')
 }
-
 function expenses() {
     return collections.expenses
 }
-
 function misc() {
     return collections.misc
 }
-
 module.exports = {
     connect,
     disconnect,
